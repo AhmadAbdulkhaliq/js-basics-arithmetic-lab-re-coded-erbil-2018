594940ea905e7f080f2bf977d0512a7dcdd86ed9
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-18 12:41:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,30 @@ Variables defined in the test suite:
 - oldID
 - currentAge
 
+
 You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+const newID = 1000000000 + oldID;
+
+if (currentAgeIsInteger) {
+       ageIsValid=true;
+      } else {
+       ageIsValid=false;
+      }
+      
+      ageIsValid=Number.isInteger(currentAge);
+      
+      
+      let randomNumber=Math.floor(Math.random() * 20) + 0;
+      
+      
+       const flooredRandomInteger = Math.floor(randomNumber);
+
+      randomInteger=Math.round(flooredRandomInteger);
+
+      
+      
+randomUserID=Number.isInteger(randomNumber)+1000000000
+      
